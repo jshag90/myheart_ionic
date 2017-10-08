3981bf5ad856e2e00471f3be0d8da11921ca84b1
jshag90 <jshag90@naver.com>
2017-10-09 01:40:03 +0900

    스케줄 조회 기능 추가 이미지 수정

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,6 @@ import { SchedulePage } from '../schedule/schedule';
 import { DrugService } from '../services/drug.service';
 
 
-
-
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
@@ -15,23 +13,44 @@ import { DrugService } from '../services/drug.service';
 export class HomePage {
     perscriptions: string[];
 
+    scheduleTermsStart: string[];
+    scheduleTermsEnd: string[];
+
+
     constructor(
         public navCtrl: NavController,
         private drugService: DrugService,
-        public platform: Platform, 
+        public platform: Platform,
         public localNotifications: LocalNotifications,
         public alertCtrl: AlertController
     ) { }
 
     ionViewWillEnter() {
+
         this.drugService.getPerscriptions().then(res => {
             this.perscriptions = res || [];
         });
+
     }
 
     //네비게이션 컨트롤러 기능 설정
     goToSchedulePage(prescriptionName) {
         this.navCtrl.push(SchedulePage, { prescriptionName: prescriptionName });
+    }
+
+    scheduleInfo(prescriptionName) {
+
+        this.drugService.getPrescription(prescriptionName).then(res => {
+            this.scheduleTermsStart = JSON.parse(res).startDate || [];
+            this.scheduleTermsEnd = JSON.parse(res).endDate || [];
+
+            var startDate = new Date(JSON.parse(res).startDate);
+            var endDate = new Date(JSON.parse(res).endDate);
+            alert("시작일 : " + startDate.getFullYear() + "-" + startDate.getMonth() + "-" + startDate.getDate()
+                + "\n종료일 : " + endDate.getFullYear() + "-" + endDate.getMonth() + "-" + endDate.getDate());
+        })
+
+
     }
 
     // 처방전을 삭제합니다.
@@ -46,7 +65,7 @@ export class HomePage {
                         if (!this.platform.is('cordova')) {
                             return this.ionViewWillEnter();
                         }
-                        
+
                         // 알람을 삭제합니다.
                         this.localNotifications.cancel(notificationIds).then(() => {
                             this.ionViewWillEnter();
